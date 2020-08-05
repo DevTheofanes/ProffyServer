e72633b532e019524b0e543c423f31a9de1ef0d3
DevTheofanes <devtheofanes@gmail.com>
2020-08-05 13:13:32 -0300

    Controller ClassesController

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  const users = [
-    {
-      name: "Theo",
-      age: 16,
-    },
-    {
-      name: "Mell",
-      age: 16,
-    },
-  ];
-
-  return res.json(users);
-});
+app.use(routes);
 
 app.listen(3333);
